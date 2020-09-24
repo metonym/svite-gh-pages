307b22c816f9fbc6904e07f0bdbce9840209a88a
Eric Liu <ericyl.us@gmail.com>
2020-09-24 14:26:17 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  const world = "world";
+</script>
+
+<style>
+  h1 {
+    color: orangered;
+  }
+</style>
+
+<h1>Hello {world}</h1>
+<p>Open App.svelte in your editor and change something to see HMR in action</p>
